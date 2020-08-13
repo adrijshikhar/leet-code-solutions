1322f9752049450f92331fc6c7aa5d085a31dbf7
Adrij Shikhar <adrijshikhar85@gmail.com>
2020-08-13 20:12:51 +0530

    Adds unique split strings

---
+++ Strings/UniqueSplitStrings.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int wiggleSort(string A) {
+  unordered_map<int, int> mp1, mp2;
+  int n = A.size(), ans = 0;
+  for (int i = 0; i < n; i++) {
+    mp2[A[i]]++;
+  }
+  for (int i = 0; i < n; i++) {
+    mp1[A[i]]++;
+    mp2[A[i]]--;
+    if (mp2[A[i]] == 0)
+      mp2.erase(A[i]);
+    if (mp1.size() == mp2.size())
+      ans++;
+  }
+
+  return ans;
+}
+
+int main() {
+  string v1 = "ababa";
+  int v2 = wiggleSort(v1);
+  cout << v2 << " ";
+
+  return 0;
+}
