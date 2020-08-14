cf8b39de30204ce75e9f1c1ab1d3c5b99528ce38
Adrij Shikhar <adrijshikhar85@gmail.com>
2020-08-14 13:52:14 +0530

    Adds longest substring with distinct char

---
+++ Strings/LongestSbStringWithKDistinctChar.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string lrsswk(string A, int k) {
+  unordered_map<int, int> mp;
+  string ans;
+  int left = 0, right = 0, n = A.size();
+  while (right < n) {
+    mp[A[right]]++;
+    while (k < mp.size()) {
+      mp[A[left]]--;
+      if (mp[A[left]] == 0)
+        mp.erase(A[left]);
+      left++;
+    }
+
+    right++;
+  }
+  for (int i = left; i <= right; i++) {
+    ans += A[i];
+  }
+  return ans;
+}
+
+int main() {
+  string v1 = "asda";
+  int k = 3;
+  string v2 = lrsswk(v1, k);
+  cout << v2 << " ";
+
+  return 0;
+}
