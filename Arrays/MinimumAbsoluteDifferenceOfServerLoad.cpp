8b78839ace160fdfcbb406080af540244e16a070
Adrij Shikhar <adrijshikhar85@gmail.com>
2020-08-15 15:53:48 +0530

    Test commit

---
+++ Arrays/MinimumAbsoluteDifferenceOfServerLoad.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+There are some processes that need to be executed. Amount of a load that process
+causes on a server that runs it, is being represented by a single integer. Total
+load caused on a server is the sum of the loads of all the processes that run on
+that server. You have at your disposal two servers, on which mentioned processes
+can be run. Your goal is to distribute given processes between those two servers
+in the way that, absolute difference of their loads will be minimized.
+
+Given an array of n integers, of which represents loads caused by successive
+processes, return the minimum absolute difference of server loads.
+
+Example 1:
+
+Input: [1, 2, 3, 4, 5]
+Output: 1
+Explanation:
+We can distribute the processes with loads [1, 2, 4] to the first server and [3,
+5] to the second one, so that their total loads will be 7 and 8, respectively,
+and the difference of their loads will be equal to 1.
+*/
+
+int minimunAmplitude(vector<int> &A) {
+  sort(A.begin(), A.end());
+  int start, end, n = A.size(), amp = A[n - 1] - A[0], k = 4, d;
+  start = 3;
+  end = n - 1;
+  for (int i = 0; i < n; i++) {
+    cout << A[i] << " ";
+  }
+  cout << endl;
+  while (k--) {
+    d = A[end] - A[start];
+    amp = min(amp, d);
+    start--;
+    end--;
+  }
+
+  return amp;
+}
+
+int main() {
+  vector<int> v1{
+      1, 1, 3, 5, 8, 8,
+  };
+  int v2 = minimunAmplitude(v1);
+  cout << v2 << " ";
+
+  return 0;
+}
